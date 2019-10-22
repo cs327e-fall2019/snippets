441e0ec39878617cb2fbe364b3139882a5f8f6ed
Shirley Cohen <scohen@cs.utexas.edu>
2019-10-22 08:01:24 -0500

    example views

--- create_views.sql
+++ create_views.sql
@@ -1,6 +1,6 @@
 -- View examples for Milestone 7
 -- Note: the project id in the FROM clause is required when creating views
-create view oscars.v_Actor_Nomination_Counts as
+create view oscars.v_Highest_Nominated_Actors as
 select entity, count(*) as nomination_count
 from `cs327e-fa2019.oscars.Nomination_Events` 
 where category like '%ACTOR%' 
@@ -8,7 +8,7 @@ group by entity
 order by nomination_count desc
 limit 30
 
-create view oscars.v_Actress_Nomination_Counts as
+create view oscars.v_Highest_Nominated_Actresses as
 select entity, count(*) as nomination_count
 from `cs327e-fa2019.oscars.Nomination_Events`  
 where category like '%ACTRESS%' 
@@ -16,16 +16,18 @@ group by entity
 order by nomination_count desc
 limit 30
 
-create view oscars.v_Director_Nomination_Counts as
-select entity, count(*) as nomination_count
+create view oscars.v_Highest_Nominated_Movies as
+select entity, year, count(*) as nomination_count
 from `cs327e-fa2019.oscars.Nomination_Events`  
-where category like '%DIRECTOR%' 
-group by entity
+where category in ('BEST MOTION PICTURE', 'BEST PICTURE', 'CINEMATOGRAPHY', 'WRITING (Story and Screenplay)', 
+	                'ART DIRECTION', 'DIRECTING', 'FILM EDITING', 'MUSIC (Original Dramatic Score)', 'MUSIC (Original Song)', 
+				     'SOUND', 'SOUND EFFECTS EDITING', 'VISUAL EFFECTS', 'MAKEUP') 
+group by entity, year
 order by nomination_count desc
 limit 30
 
--- Query the views: 
--- select * from oscars.v_Actor_Nomination_Counts
--- select * from oscars.v_Actress_Nomination_Counts
--- select * from oscars.v_Director_Nomination_Counts
+-- Querying the views: 
+-- select * from oscars.v_Highest_Nominated_Actors
+-- select * from oscars.v_Highest_Nominated_Actresses
+-- select * from oscars.v_Highest_Nominated_Movies
 
